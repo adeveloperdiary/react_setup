ce17623ac13d5561f5c1e9e99d1fd96d0ed84fda
Abhisek Jana <adeveloperdiary@gmail.com>
2016-02-21 22:37:29 -0500

    Initial Commit

---
+++ ES6/lib/app.jsx
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>React Tutorial - ADeveloperDiary.com</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
